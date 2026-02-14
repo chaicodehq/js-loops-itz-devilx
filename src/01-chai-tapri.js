c56cc8449f03d832d0f9233064b84e798c0b4951
itz-devilx <intekhab118211989@gmail.com>
2026-02-14 19:08:34 +0530

    implement chai-tapri

--- src/01-chai-tapri.js
+++ src/01-chai-tapri.js
@@ -27,5 +27,24 @@
  *   // => { totalChai: 0, totalRevenue: 0 }
  */
 export function chaiTapriRevenue(customers) {
-  // Your code here
+  if(!Number.isInteger(customers) || customers <= 0){
+    return {totalChai: 0, totalRevenue: 0}
+  }
+  let totalRevenue = 0;
+  let totalChai = customers;
+
+  // better solution -- 
+  // let adrak = Math.floor(totalChai / 3);
+  // let cutting = totalChai - adrak;
+  // totalRevenue = (adrak * 15) + (cutting * 10);
+
+  for(let i=1; i<=customers; i++){
+    if(i % 3 === 0){
+      totalRevenue += 15;
+    }else{
+      totalRevenue += 10;
+    }
+  }
+  
+  return {totalChai, totalRevenue}
 }
